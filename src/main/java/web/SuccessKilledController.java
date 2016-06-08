ef0195494778638894ccc54d3b51e33251aff88e
wangyang <541230721@qq.com>
2016-06-08 11:32:40 +0800

    秒杀成功明细Controller

---
+++ src/main/java/web/SuccessKilledController.java
@@ -0,0 +1,22 @@
+package web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import service.SuccesskillService;
+
+/**
+ * Created by admin on 2016/6/8.
+ */
+@Controller
+@RequestMapping("/successKilled")
+public class SuccessKilledController {
+
+      private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+      @Autowired
+      private SuccesskillService successkillService;
+
+}
